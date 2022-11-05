8ae52f9f8f1f52ff30384bea902a65ad10347df0
Owen <47730337+owenpa@users.noreply.github.com>
2022-11-04 22:25:00 -0500

    Update main.js

--- soundcloudextension/main.js
+++ soundcloudextension/main.js
@@ -1,6 +1,3 @@
-/* The site is constantly refreshing as you scroll down.
- * We want to delete the class "soundList__item" after evaluating whether or not it is a repost (check extension settings as to whether or not to include tracks/playlists that the creator of the song is promoting, whether or not the person reposting is a feature on the song, and whether or not the application is even turned on, before deleting). Turning off/on the application will most likely prompt a refresh considering I don't know how to completely delete a listing, store it, restore it when the extension is disabled, and not blow up the ram
- */
 let value;
 const sleep = (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
