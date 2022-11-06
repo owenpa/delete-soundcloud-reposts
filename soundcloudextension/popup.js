7ef1b6da18cabc2261c0c669ce69a6e7252c2d43
Owen <47730337+owenpa@users.noreply.github.com>
2022-11-06 01:08:03 -0500

    Added functionality for reading extension input
    I'm currently experimenting with saving the extension configuration and redoing the file

--- soundcloudextension/popup.js
+++ soundcloudextension/popup.js
@@ -1,18 +1,46 @@
-document.querySelector("#enabled").addEventListener("click", function() {
+document.querySelector("#running").addEventListener("click", function() {
     toggleEnabled(false);
 });
 
-document.querySelector("#disabled").addEventListener("click", function() {
+document.querySelector("#notrunning").addEventListener("click", function() {
     toggleEnabled(true);
 });
-
-function toggleEnabled(status) {
-    chrome.storage.sync.set({
-        enabled: status
+document.querySelectorAll("input").forEach(event => {
+    event.addEventListener("click", function(e) {
+        toggleEnabled(event.checked, "input")
     })
-    document.querySelector("#enabled").classList.toggle("hide", !status);
-    document.querySelector("#disabled").classList.toggle("hide", status);
-    changeUI(status)
+})
+
+function toggleEnabled(status, inputs = null) {
+    if (inputs != null) {
+        butt = document.querySelector("button:not(.hide)").id
+        butt === "running" ? status = true : status = false
+
+        let form = document.querySelectorAll("input");
+        let repost = form.item(0).checked
+        let features = form.item(1).checked
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { settings: [status, repost, features] }, function(response) {
+                return true;
+            });
+        });
+    } else {
+        chrome.storage.sync.set({
+            enabled: status
+        })
+        document.querySelector("#running").classList.toggle("hide", !status);
+        document.querySelector("#notrunning").classList.toggle("hide", status);
+        let form = document.querySelectorAll("input");
+        let repost = form.item(0).checked
+        let features = form.item(1).checked
+
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { settings: [status, repost, features] }, function(response) {
+                return true;
+            });
+        });
+    }
+
 }
 
 function restore_options() {
@@ -20,14 +48,6 @@ function restore_options() {
         enabled: true
     }, function(storage) {
         toggleEnabled(storage.enabled)
-    });
-}
-
-function changeUI(input) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { greeting: input }, function(response) {
-            return true;
-        });
     });
 }
 
