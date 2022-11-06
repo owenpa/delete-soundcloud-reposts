615a33a953876f003ad85fb2d322ae7bed159d28
Owen <47730337+owenpa@users.noreply.github.com>
2022-11-06 01:10:46 -0500

    Added configuration options
    You can now remove every repost, only reposts by the creator of the song, only reposts where the reposter is a feature on the song, and only reposts that don't fall into either category.
    
    I have yet to view the performance and optimizing this.

--- soundcloudextension/main.js
+++ soundcloudextension/main.js
@@ -1,28 +1,83 @@
-let value;
+let deleteStatus;
 const sleep = (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
-async function delRepost() {
+async function delRepost(reposts = true, features = true) {
     let stream = document.querySelector(".soundList__item");
     if (stream != null) {
-        while (value === true) {
-            let repost = document.querySelectorAll(".soundContext__repost")
-            repost.forEach((post) => {
-                post.closest(".soundList__item").remove()
-            })
-            await sleep(1000);
+        if (reposts === true && features === true) {
+            while (deleteStatus === true) {
+                let repost = document.querySelectorAll(".soundContext__repost")
+                repost.forEach((post) => {
+                    post.closest(".soundList__item").remove()
+                })
+                await sleep(1000);
+            }
+        } else if (reposts === true && features === false) {
+            while (deleteStatus === true) {
+                let repost = document.querySelectorAll(".soundContext__repost")
+                repost.forEach((post) => {
+                    let reposter = post.previousElementSibling.textContent.toLowerCase()
+                    let artist = post.closest(".sound").querySelector(".soundTitle__usernameText").textContent.toLowerCase()
+                    if (artist.includes(reposter)) {
+                        post.closest(".soundList__item").remove()
+                    }
+                })
+                await sleep(1000);
+            }
+        } else if (reposts === false && features === true) {
+            while (deleteStatus === true) {
+                let repost = document.querySelectorAll(".soundContext__repost")
+                repost.forEach((post) => {
+                    let artist = post.previousElementSibling.textContent.toLowerCase();
+                    if (post.closest(".visualSound") === null) {
+                        let title = post.closest(".sound").querySelector(".soundTitle__title").textContent.toLowerCase();
+                        if (title.includes(artist)) {
+                            post.closest(".soundList__item").remove()
+                        }
+                    } else {
+                        let title = post.closest(".visualSound").querySelector(".soundTitle__title").title.toLowerCase(); // for the songs with special backgrounds
+                        if (title.includes(artist)) {
+                            post.closest(".soundList__item").remove()
+                        }
+                    }
+                })
+                await sleep(1000);
+            }
+        } else if (reposts === false && features === false) {
+            while (deleteStatus === true) {
+                let repost = document.querySelectorAll(".soundContext__repost")
+                repost.forEach((post) => {
+                    let reposter = post.previousElementSibling.textContent.toLowerCase();
+                    let artist = post.closest(".sound").querySelector(".soundTitle__usernameText").textContent.toLowerCase()
+                    if (post.closest(".visualSound") === null) {
+                        let title = post.closest(".sound").querySelector(".soundTitle__title").textContent.toLowerCase();
+                        if (title.includes(reposter) || artist.includes(reposter)) {
+
+                        } else {
+                            post.closest(".soundList__item").remove()
+                        }
+                    } else {
+                        let title = post.closest(".visualSound").querySelector(".soundTitle__title").title.toLowerCase();
+                        if (title.includes(reposter) || artist.includes(reposter)) {
+
+                        } else {
+                            post.closest(".soundList__item").remove()
+                        }
+                    }
+                })
+                await sleep(1000);
+            }
         }
-    } else {
-        await sleep(5000);
-        delRepost()
     }
 }
-
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        value = request.greeting;
-        sendResponse({ bye: "sent" });
-        delRepost();
+        deleteStatus = request.settings[0]
+        repostStatus = request.settings[1]
+        featureStatus = request.settings[2]
+        delRepost(repostStatus, featureStatus);
+        sendResponse("sent")
     }
 );
