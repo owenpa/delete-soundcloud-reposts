7f61fb21ca613e920420ac398f564cf8727e240d
Owen <47730337+owenpa@users.noreply.github.com>
2022-11-10 12:22:44 -0600

    Remade popup.js + save config

--- soundcloudextension/popup.js
+++ soundcloudextension/popup.js
@@ -1,54 +1,67 @@
-document.querySelector("#running").addEventListener("click", function() {
-    toggleEnabled(false);
-});
-
-document.querySelector("#notrunning").addEventListener("click", function() {
-    toggleEnabled(true);
-});
-document.querySelectorAll("input").forEach(event => {
-    event.addEventListener("click", function(e) {
-        toggleEnabled(event.checked, "input")
+document.querySelectorAll("input, #running, #notrunning").forEach(event => {
+    event.addEventListener("click", function() {
+        if (event.id === "notrunning" || event.id === "running") {
+            let repost = document.querySelector("#reposter").checked
+            let features = document.querySelector("#featuring").checked
+            butt = document.querySelector("button:not(.hide)").id
+            butt === "running" ? enabled = false : enabled = true
+            values = [enabled, repost, features]
+            toggleEnabled(values)
+        } else if (event.id === "reposter" || event.id === "featuring") {
+            let repost = document.querySelector("#reposter").checked
+            let features = document.querySelector("#featuring").checked
+            butt = document.querySelector("button:not(.hide)").id
+            butt === "running" ? enabled = true : enabled = false
+            values = [enabled, repost, features]
+            toggleEnabled(values, enabled)
+        }
     })
 })
 
-function toggleEnabled(status, inputs = null) {
-    if (inputs != null) {
-        butt = document.querySelector("button:not(.hide)").id
-        butt === "running" ? status = true : status = false
-
-        let form = document.querySelectorAll("input");
-        let repost = form.item(0).checked
-        let features = form.item(1).checked
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { settings: [status, repost, features] }, function(response) {
-                return true;
-            });
-        });
-    } else {
-        chrome.storage.sync.set({
-            enabled: status
+function toggleEnabled(status, button = null) {
+    chrome.storage.local.set({
+        enabled: status[0],
+        repost: status[1],
+        features: status[2]
+    })
+    if (button === null) {
+        chrome.storage.local.get(["enabled", "repost", "features"], function(result) {
+            document.querySelector("#running").classList.toggle("hide", !status[0]);
+            document.querySelector("#notrunning").classList.toggle("hide", status[0]);
+            document.querySelector("#reposter").checked = status[1]
+            document.querySelector("#featuring").checked = status[2]
         })
-        document.querySelector("#running").classList.toggle("hide", !status);
-        document.querySelector("#notrunning").classList.toggle("hide", status);
-        let form = document.querySelectorAll("input");
-        let repost = form.item(0).checked
-        let features = form.item(1).checked
-
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { settings: [status, repost, features] }, function(response) {
-                return true;
-            });
-        });
+        sendMessage(status)
+    } else {
+        document.querySelector("#reposter").checked = status[1]
+        document.querySelector("#featuring").checked = status[2]
+        sendMessage(status)
     }
 
+
+    let form = document.querySelectorAll("input");
+    let repost = form.item(0).checked
+    let features = form.item(1).checked
 }
 
-function restore_options() {
-    chrome.storage.sync.get({
-        enabled: true
+function loadOptions() {
+    chrome.storage.local.get({
+        enabled: false,
+        repost: false,
+        features: false
     }, function(storage) {
-        toggleEnabled(storage.enabled)
-    });
+        values = [storage.enabled, storage.repost, storage.features]
+        toggleEnabled(values)
+    })
 }
 
-document.addEventListener("DOMContentLoaded", restore_options);
+function sendMessage(message) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { settings: [message[0], message[1], message[2]] }, function(response) {
+            console.debug(response)
+            return true;
+        });
+    });
+
+}
+document.addEventListener("DOMContentLoaded", loadOptions);
